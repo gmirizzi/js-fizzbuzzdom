f6c37fc4e84c2984560ef4ee7880b833bff034d9
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-02-23 16:04:38 +0100

    cambiati background colors

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 5 == 0 & i % 3 == 0) {
         element.innerHTML = 'FizzBuzz';
+        element.classList.add('fizzbuzz');
     } else if (i % 5 == 0) {
         element.innerHTML = 'Buzz';
+        element.classList.add('buzz');
     } else if (i % 3 == 0) {
         element.innerHTML = 'Fizz';
+        element.classList.add('fizz');
     } else {
         element.innerHTML = i;
     }
