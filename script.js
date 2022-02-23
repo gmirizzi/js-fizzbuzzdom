e08198122bbcd7a91760290b90e34b117324b0c2
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-02-23 15:07:55 +0100

    creato il ciclo js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-for (let i = 0; i <= 100; i++) {
-    const element = document.createElement ('div');
+for (let i = 1; i <= 100; i++) {
+    const element = document.createElement('div');
     element.classList.add('box');
-    
 
+    if (i % 5 == 0 & i % 3 == 0) {
+        element.innerHTML = 'FizzBuzz';
+    } else if (i % 5 == 0) {
+        element.innerHTML = 'Buzz';
+    } else if (i % 3 == 0) {
+        element.innerHTML = 'Fizz';
+    } else {
+        element.innerHTML = i;
+    }
+
+    console.log(element);
 }
